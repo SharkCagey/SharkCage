ea5f443fb4a99420bb2b6b8d9dcdd03075396b63
etgramli <etgramlich@gmx.de>
2016-12-08 11:11:07 +0100

    Added own project for native tests, but still does not execute

---
+++ Shark Cage/UnitTest/TestTemplate.cpp
@@ -0,0 +1,12 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest {
+	TEST_CLASS(UnitTest) {
+	public:
+		TEST_METHOD(TestTemplate_TestMethod1) {
+			// TODO: Your test code here
+		}
+	};
+}
