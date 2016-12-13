3508cfce55fb6d58afd6b26c8444e873d7a2b9a7
etgramli <etgramlich@gmx.de>
2016-12-13 12:45:31 +0100

    Added file selecotr dialog to choose exe to run in cage maneger

--- Shark Cage/StarterCMD/StarterCMD.cpp
+++ Shark Cage/StarterCMD/StarterCMD.cpp
@@ -4,12 +4,12 @@
 #include "stdafx.h"
 #include "../Cage service/NetworkManager.h"
 #include "../Cage service/MSG_Service.h"
-
+#include <Windows.h>
+#include <Commdlg.h>
 
 int main() {
     NetworkManager mgr(UI);
 
-    
 
 	int msgboxID = MessageBox(
 		NULL,
@@ -20,13 +20,51 @@ int main() {
 
 	switch (msgboxID) {
 	case IDNO:
-		return 0; // Just quit
+		return -1; // Just quit
 	case IDYES:
         mgr.send(MSG_TO_SERVICE_toString(START_CM));
 		break;
 	}
+    
 
-    // Show file selector dialog and send path to the cage service
+    TCHAR filename[MAX_PATH] = {0};
+    TCHAR filter[MAX_PATH] = 
+    {
+        TEXT("Executables (.exe)\0*.exe\0")
+        TEXT("All Files\0*.*\0")
+    };
+
+    OPENFILENAME ofn = {0};
+    TCHAR _sbuf[MAX_PATH];
+    TCHAR *initDir = _sbuf;
+
+    ofn.lStructSize         = sizeof(OPENFILENAME);
+    ofn.lpstrFilter         = filter;
+    ofn.nFilterIndex        = 1;
+    ofn.lpstrFile           = filename;
+    ofn.nMaxFile            = MAX_PATH;
+    ofn.lpstrInitialDir     = initDir;
+    ofn.lpstrTitle          = L"Select program to run in Cage Manager";
+    ofn.Flags               = OFN_EXPLORER | OFN_HIDEREADONLY | OFN_NONETWORKBUTTON | OFN_NOREADONLYRETURN | OFN_FILEMUSTEXIST;
+
+    if (!GetOpenFileName(&ofn)) {
+        if (CommDlgExtendedError() == 0) {
+            return 0;
+        }
+        return -1;
+    }
+    std::cout << "Selected file: " << filename << std::endl;
+
+    ///////////////// A bit too simple, should be replaces
+    std::string convertedFilename;
+    for (char c : filename) {
+        convertedFilename += c;
+    }
+    ////////////////
+
+    mgr.send(MSG_TO_SERVICE_toString(START_PC) + convertedFilename);
+
+
 
     return 0;
 }
