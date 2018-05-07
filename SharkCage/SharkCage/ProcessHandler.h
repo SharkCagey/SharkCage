f76af639748bf5c6d69e87be92ab25b8085354c0
Jonas Reinwald <jonas.reinwald@htwg-konstanz.de>
2018-05-07 21:42:20 +0200

    removed the shark cage project which wasn't used anyway

--- SharkCage/SharkCage/ProcessHandler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class ProcessHandler
-{
-public:
-	/*
-	 * Creates and starts a new process.
-	 */
-	void CreateProcess(const std::wstring &desktop_name /*, SECURITY_DESCRIPTOR sd */);	// Add argument to specify programm to be started
-};
